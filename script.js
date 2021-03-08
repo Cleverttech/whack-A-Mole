be645fd9ee3e030a6bcbdf2aa21a7e03a7f375e4
Cleverttech <dzifatedeku@gmail.com>
2021-03-08 14:19:52 +0100

    add 3 more holes and peek function

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let previousHole;
+let stopTime = false;
 
 function randomTime(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+  if (hole === previousHole) {
+    return randomHole(holes); //repeat fnx..recursion
+  }
+
+  previousHole = hole;
+  return hole;
 }
